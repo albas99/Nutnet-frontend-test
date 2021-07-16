f31feefae5d76b6df967d3186a5dad058bf81421
Faisal Albasu <albasfaisal@yandex.ru>
2021-07-17 01:33:51 +0400

    fixed mobile responsiveness

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ function openNavigation() {
         navList.style.display = 'none';
     }
 }
+
+let videoModal = document.querySelector('.video-modal');
+let videoButton = document.querySelector('.video-button');
+let closeModalButton = document.querySelector('.close');
+
+videoButton.addEventListener('click', displayVideo);
+closeModalButton.addEventListener('click', closeModal);
+
+function displayVideo() {
+    videoModal.style.display = 'block';
+}
+
+function closeModal() {
+    videoModal.style.display = 'none';
+}
+
+window.onclick = function (event) {
+  if (event.target == videoModal) {
+    videoModal.style.display = "none";
+  }
+};
